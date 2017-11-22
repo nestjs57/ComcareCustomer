30263cf9a7b4f2c34b37308dc44c63be1d7a090b
Attachai <sawangwong_a@silpakorn.edu>
2017-11-23 00:02:14 +0700

    ทำหน้าสรุปข้อมูล

---
+++ app/src/main/java/com/comcare/comcarecustomer/SumDetail.java
@@ -0,0 +1,13 @@
+package com.comcare.comcarecustomer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SumDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sum_detail);
+    }
+}
