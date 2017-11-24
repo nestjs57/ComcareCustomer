bf8118b8e46e45f74a3a1ea2774532eedae00076
peetoey <jantaraplin_s@silpakorn.edu>
2017-11-25 01:31:03 +0700

    Dialog ช่องว่าง Register&Login with Email

--- app/src/main/java/com/comcare/comcarecustomer/LoginAndRegister/EmailLogin.java
+++ app/src/main/java/com/comcare/comcarecustomer/LoginAndRegister/EmailLogin.java
@@ -1,7 +1,9 @@
 package com.comcare.comcarecustomer.LoginAndRegister;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,6 +21,7 @@ public class EmailLogin extends AppCompatActivity {
 
     EditText getMail,getPass;
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,35 +34,49 @@ public class EmailLogin extends AppCompatActivity {
 
     public void onClickLogin(View obj) {
 
-        String email = getMail.getText().toString();
-        String password = getPass.getText().toString();
+        if (getMail.getText().toString().trim().equals("")&&getPass.getText().toString().trim().equals("")){
+            Dialog("\n- อีเมล\n- รหัสผ่าน");
+        }else if (getMail.getText().toString().trim().equals("")){
+            Dialog("\n- อีเมล");
+        }else if (getPass.getText().toString().trim().equals("")){
+            Dialog("\n- รหัสผ่าน");
+        }else {
 
-            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
+            firebaseAuth.signInWithEmailAndPassword(getMail.getText().toString(), getPass.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
 
-                    if (!task.isSuccessful()) {
-                        Toast.makeText(EmailLogin.this, "Login Failed Please Try Agian", Toast.LENGTH_LONG).show();
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(EmailLogin.this, "Login Failed Please Try Agian", Toast.LENGTH_LONG).show();
 
-                    } else {
-                        Toast.makeText(EmailLogin.this, "Login Successful", Toast.LENGTH_LONG).show();
+                            } else {
+//                        Toast.makeText(EmailLogin.this, "Login Successful", Toast.LENGTH_LONG).show();
 
-                        Intent intent = new Intent(EmailLogin.this, MainActivity.class);
-                        finish();
-                        startActivity(intent);
-                    }
-
-                }
-            });
-
+                                intent = new Intent(EmailLogin.this, MainActivity.class);
+                                finish();
+                                startActivity(intent);
+                            }
+                        }
+                    });
+        }
     }
-
     private void bindWidget() {
 
         getMail = (EditText) findViewById(R.id.edtMail);
         getPass = (EditText) findViewById(R.id.edtPass);
 
     }
+    private void Dialog(String txt) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("คุณยังไม่ได้กรอก: " + txt);
+        builder.setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.show();
+    }
+
 
 
 }
--- app/src/main/java/com/comcare/comcarecustomer/LoginAndRegister/EmailRegister.java
+++ app/src/main/java/com/comcare/comcarecustomer/LoginAndRegister/EmailRegister.java
@@ -1,7 +1,9 @@
 package com.comcare.comcarecustomer.LoginAndRegister;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +25,8 @@ public class EmailRegister extends AppCompatActivity {
     private EditText getEmail;
     private EditText getPassword;
     private EditText getConfirmPassword;
+    FirebaseAuth firebaseAuth;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,43 +38,49 @@ public class EmailRegister extends AppCompatActivity {
 
     public void onClickRegister(View obj){
 
-            final String name = getName.getText().toString();
-            final String email = getEmail.getText().toString();
-            String password = getPassword.getText().toString();
+//            String name = getName.getText().toString();
+//            String email = getEmail.getText().toString();
+//            String password = getPassword.getText().toString();
 
-            final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
 
-                    if (!task.isSuccessful()) {
-                        Toast.makeText(EmailRegister.this, "Register Failed", Toast.LENGTH_LONG).show();
+            if (getName.getText().toString().trim().equals("")
+                    &&getEmail.getText().toString().trim().equals("")
+                    &&getPassword.getText().toString().trim().equals("")){
+                Dialog("\n- ชื่อผู้ใช้งาน\n- อีเมล\n- รหัสผ่าน");
+            }else if (getName.getText().toString().trim().equals("")){
+                Dialog("\n- ชื่อผู้ใช้งาน");
+            }else if (getEmail.getText().toString().trim().equals("")){
+                Dialog("\n- อีเมล");
+            }else if (getPassword.getText().toString().trim().equals("")){
+                Dialog("\n- รหัสผ่าน");
+            }else {
+                firebaseAuth = FirebaseAuth.getInstance();
+                firebaseAuth.createUserWithEmailAndPassword(getName.getText().toString(), getPassword.getText().toString())
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
 
-                    } else {
-                        Toast.makeText(EmailRegister.this, "Register Successful", Toast.LENGTH_LONG).show();
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(EmailRegister.this, "Register Failed", Toast.LENGTH_LONG).show();
 
-                        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                        DatabaseReference dbref = FirebaseDatabase.getInstance().getReference();
-                        DatabaseReference childref = dbref.child("user");
-                        childref.child("" + user.getUid()).child("Info").child("full_name").setValue(name);
-                        childref.child("" + user.getUid()).child("Info").child("email").setValue(email);
+                        } else {
+//                            Toast.makeText(EmailRegister.this, "Register Successful", Toast.LENGTH_LONG).show();
 
-                        Intent intent = new Intent(getApplication(), EmailLogin.class);
-                        finish();
-                        startActivity(intent);
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                            DatabaseReference dbref = FirebaseDatabase.getInstance().getReference();
+                            DatabaseReference childref = dbref.child("user");
+                            childref.child("" + user.getUid()).child("Info").child("full_name").setValue(getName.getText().toString());
+                            childref.child("" + user.getUid()).child("Info").child("email").setValue(getEmail.getText().toString());
+
+                            intent = new Intent(getApplication(), EmailLogin.class);
+                            finish();
+                            startActivity(intent);
+
+                        }
                     }
-                }
 
-            });
-
-    }
-
-    public void onClickCancel(View obj){
-
-//        Intent doIntent = new Intent(RegisterActivity.this, UserInfomation.class);
-//        finish();
-//        startActivity(doIntent);
-
+                });
+            }
     }
 
     private void bindWidget() {
@@ -80,6 +90,15 @@ public class EmailRegister extends AppCompatActivity {
         getPassword = (EditText) findViewById(R.id.edtPass);
         getConfirmPassword = (EditText) findViewById(R.id.edtConfirmPass);
 
+    }
+    private void Dialog(String txt) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("คุณยังไม่ได้กรอก: " + txt);
+        builder.setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.show();
     }
 
 
